46b3fafea8b917e1a8bae57693b2c0d32f6edd0e
Harry Buxton <92524519+hbux@users.noreply.github.com>
2022-03-10 10:53:49 +0000

    Add invoice modal
    Adds the ability to display the invoice uploaded using a modal, adds wireframes and updates readme.

---
+++ ElancoUI/wwwroot/js/main.js
@@ -0,0 +1,19 @@
+﻿
+var invoiceModal = document.querySelector(".modal-invoice");
+
+function toggleInvoiceModal() {
+    if (document.querySelector(".modal-invoice").style.display === "block") {
+        document.querySelector(".modal-invoice").style.display = "none";
+    }
+    else {
+        document.querySelector(".modal-invoice").style.display = "block";
+    }
+}
+
+async function setImageUsingStreaming(imageElementId, imageStream) {
+    const arrayBuffer = await imageStream.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+
+    document.getElementById(imageElementId).src = url;
+}
