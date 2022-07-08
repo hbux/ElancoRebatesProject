15298111d6c83a46ed2a580ccb9a1840e71ba3de
Harry Buxton <92524519+hbux@users.noreply.github.com>
2022-07-08 08:09:08 +0100

    Remove redundant test cases

--- ElancoLibrary.Tests/OfferFilterTests.cs
+++ ElancoLibrary.Tests/OfferFilterTests.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace ElancoLibrary.Tests
 {
     public class OfferFilterTests
     {
+        [Fact]
         public void AutoMatchOffer_ShouldMatchOfferCorrectly()
         {
-
+            throw new NotImplementedException();
         }
 
+        [Fact]
         public void AutoMatchOffer_ShouldThrowNullReferenceException()
         {
-
+            throw new NotImplementedException();
         }
     }
 }
